7171d8b4e2d76b2eea86e364a275118033f98401
oRFolkz <FabioFernandes2255@gmail.com>
2024-11-20 21:12:50 -0300

    Lancamento 1.0

--- src/components/PagesProdutos/BannerProduto/index.tsx
+++ src/components/PagesProdutos/BannerProduto/index.tsx
@@ -1,24 +1,13 @@
 import { Banner, NomeDoProduto, TagDoProduto, MaxWidth } from './styles'
-import API from '../../API/api'
-import { useRestaurant } from '../../Providers/RestauranteIDProvider/RestauranteIDProvider'
-import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function BannerProduto() {
-  const { restaurantesAPI } = API()
-  const { idRestaurante } = useRestaurant()
-  const [tipoDoRestaurante, setTipoDoRestaurante] = useState<string>('')
-
-  useEffect(() => {
-    if (restaurantesAPI.length > 0) {
-      const grabTipoDoRestaurante = restaurantesAPI[(Number(idRestaurante) - 1)].tipo
-      setTipoDoRestaurante(grabTipoDoRestaurante)
-    }
-  }, [restaurantesAPI, tipoDoRestaurante])
+  const { title } = useParams()
 
   return (
     <Banner>
       <MaxWidth>
-        <TagDoProduto>{tipoDoRestaurante ? tipoDoRestaurante : '...'}</TagDoProduto>
+        <TagDoProduto>{title}</TagDoProduto>
         <NomeDoProduto>La dolce vita trattoria</NomeDoProduto>
       </MaxWidth>
     </Banner>
